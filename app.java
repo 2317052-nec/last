337667f4842af70ebab89c1a4501fe4c3a70b0e1
2317052-nec <2317052@nec.edu.in>
2025-10-27 17:58:38 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,8 @@
+public class app{
+public static void main(String[] args){
+ System.out.println("to-do list");
+ System.out.println("list of tasks:");
+System.out.println("reading");
+System.out.println("writing");
+
+}
